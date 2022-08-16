53a8fc2e3cb67c8f0d532c1dee1a5820c47995ba
MagicManatee <arthur.ibarra@edu.pucrs.br>
2022-08-16 10:28:11 -0300

    Uhuu, fixed if/else/endifs.

--- main.go
+++ main.go
@@ -3,27 +3,29 @@ package main
 import (
 	"islash/modules/interpreter"
 	"islash/modules/lexer"
+	"log"
+	"os"
 	"path/filepath"
+	"strings"
 )
 
 func main() {
 	/*
 		Finding the program path.
 	*/
-	/*
-		if len(os.Args) < 2 {
-			log.Fatal("No program name specified.")
-		}
-		if !strings.HasSuffix(os.Args[1], ".isl") {
-			log.Fatal("Your program extension must be '.isl'.")
-		}
-		cwd, err := os.Getwd()
-		if err != nil {
-			log.Fatal("Unnable to get user's current directory.")
-		}
-		sourceCodePath := filepath.Join(cwd, os.Args[1])
-	*/
-	sourceCodePath := "testif.isl"
+
+	if len(os.Args) < 2 {
+		log.Fatal("No program name specified.")
+	}
+	if !strings.HasSuffix(os.Args[1], ".isl") {
+		log.Fatal("Your program extension must be '.isl'.")
+	}
+	cwd, err := os.Getwd()
+	if err != nil {
+		log.Fatal("Unnable to get user's current directory.")
+	}
+	sourceCodePath := filepath.Join(cwd, os.Args[1])
+
 	/*
 		Mounting tokens with the lexer.
 	*/
